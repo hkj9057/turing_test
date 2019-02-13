7975dc683074560fb55515630879cb242a4b7c23
at08 <atc9057@naver.com>
2019-02-13 13:26:22 +0900

    change

---
+++ a_02_13.py
@@ -0,0 +1,49 @@
+import pygame
+import time
+
+
+
+#key, value
+a = {'title': 'a' , 'list_num' : 1, 'location' : '/home/at08/a.mp3', 'like' : 42, 'name' : 'heo gak'}
+b = {'title': 'b' , 'list_num' : 11, 'location' : '/home/at08/a.mp3', 'like' : 1001, 'name' : 'heo gak'}
+c = {'title': 'c' , 'list_num' : 111, 'location' : '/home/at08/a.mp3', 'like' : 84, 'name' : 'heo gak'}
+d = {'title': 'd' , 'list_num' : 1111, 'location' : '/home/at08/a.mp3', 'like' : 1, 'name' : 'heo gak'}
+e = {'title': 'e' , 'list_num' : 11111, 'location' : '/home/at08/a.mp3', 'like' : 104, 'name' : 'heo gak'}
+
+music_list = [a,b,c,d,e]
+#p = 1이면, 좋아요수, p = 2라면 플레이횟수
+p = input()
+
+def like_sort(p,music_list):
+
+    for i in range(len(music_list)):
+
+        for j in range(len(music_list)-1):
+
+            if(music_list[j]['like'] < music_list[j+1]['like']):
+                music_list[j],music_list[j+1] = music_list[j+1],music_list[j]
+
+
+def list_num_sort(p,music_list):
+
+    for i in range(len(music_list)):
+
+        for j in range(len(music_list)-1):
+
+            if(music_list[j]['list_num'] < music_list[j+1]['list_num']):
+                music_list[j],music_list[j+1] = music_list[j+1],music_list[j]
+
+if (p == 'like'):
+    like_sort(p,music_list)
+elif(p == 'list_num'):
+    list_num_sort(p,music_list)
+
+print(music_list)
+
+pygame.init()
+
+pygame.mixer.music.load(music_list[0]['location'])
+
+pygame.mixer.music.play()
+
+time.sleep(10)
